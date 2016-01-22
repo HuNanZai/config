5ad4312d85b1aa685b5a87faf360f6026becb9ae
HuNanZai <123165587@qq.com>
2016-01-22 18:04:26 +0800

    Initial commit

---
+++ src/Exception/FileNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HuNanZai\Component\Config\Exception;
+
+/**
+ * Class FileNotFoundException 文件没有找到的异常
+ *
+ * @package HuNanZai\Component\Config\Exception
+ */
+class FileNotFoundException extends \Exception
+{
+
+}
---
+++ src/Exception/UnsupportInputException.php
@@ -0,0 +1,12 @@
+<?php
+namespace HuNanZai\Component\Config\Exception;
+
+/**
+ * Class UnsupportInputException    //不支持的输入的错误
+ *
+ * @package HuNanZai\Component\Config\Exception
+ */
+class UnsupportInputException extends \Exception
+{
+
+}
